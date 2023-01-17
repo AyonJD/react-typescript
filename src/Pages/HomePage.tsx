619f0ed3ff109211fd75e42348fd354aa21249f9
Ayon Jodder <83225232+AyonJD@users.noreply.github.com>
2023-01-17 22:10:46 +0600

    Product card

--- src/Pages/HomePage.tsx
+++ src/Pages/HomePage.tsx
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Navbar from '../Components/Shared/Navbar';
 
 const HomePage = () => {
+    const [products, setProducts] = useState([]);
+    // useEffect(() => {
+    //     fetch(`http://localhost:5000/products`)
+    //         .then(res => res.json())
+    //         .then(data => setProducts(data))
+    // }, [])
+
+    // console.log(products)
     return (
         <div>
             <Navbar />
+
+
         </div>
     );
 };
